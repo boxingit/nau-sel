f8fd99e731601dbc98f7c2a1955c1bac78f9e88f
boxingit <sbuxamzn@gmail.com>
2020-07-05 12:46:40 -0500

    Ab test

--- src/test/java/scenarios/ABTesting.java
+++ src/test/java/scenarios/ABTesting.java
@@ -7,9 +7,13 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class ABTesting extends BasePage {
 
 
+    // TODO add java doc
     @Test
     public void testPageTitle() {
         driver.get("http://the-internet.herokuapp.com/");
@@ -17,10 +21,20 @@ public class ABTesting extends BasePage {
         abTesting.click();
         WebElement header = driver.findElement(By.tagName("h3"));
         String actualHeader = header.getText();
-        String expectedHeader = "A/B Test Control";
-        Assert.assertEquals(actualHeader, expectedHeader, "Header value is not right!");
+        String expectedHeader = "A/B Test Control"; // A/B Test Variation 1
+        //Assert.assertEquals(actualHeader, expectedHeader, "Header value is not right!");
+
+        Assert.assertTrue(actualHeader.contains("A/B Test"));
+        Assert.assertTrue(actualHeader.startsWith("A/B Test"));
+
 
         //TODO: Above use hamcrest assertion as header value is dynamic
+        assertThat(actualHeader, containsString("A/B Test"));
+        assertThat(actualHeader, startsWith("A/B Test"));
+        //assertThat(actualHeader, endsWith("A/B Test"));
+        //assertThat(actualHeader, is("A/B Test"));
+
+
 
     }
 }
